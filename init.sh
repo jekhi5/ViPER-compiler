794dbc68113d64b835a7ea038c3866efee5adcb6
Emery <emery.jacobowitz@gmail.com>
2025-01-17 00:14:47 +0000

    HW init script

---
+++ init.sh
@@ -0,0 +1,28 @@
+curl -L $1 -o HW12345.html
+repo=`grep -o 'cs4410/[^"]*' HW12345.html`
+dirname="${repo:7}"
+repolink="git@github.khoury.northeastern.edu:${repo}.git"
+    
+rm HW12345.html
+git clone ${repolink}
+
+files=`ls ${dirname}/*.ml`
+
+script="if dune build; then
+    make clean
+    make test
+    ./test
+    make clean
+
+    if [ \$# -gt 0 ]; then
+        zip submission.zip Makefile ${files}
+        echo 'Saved submission.zip'
+    fi
+else
+    echo 'Build failure. Aborting...'
+fi"
+echo "${script}" > $dirname/run.sh
+chmod 777  $dirname/run.sh
+
+rm -rf "${dirname}.git"
+mv $dirname "$2/${dirname}"
