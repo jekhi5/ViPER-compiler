1453e638a07caae4b76adb690ba77091045d6c90
Jacob Kline <jacobk513@gmail.com>
2025-02-24 14:34:05 -0500

    updated gen_tests script

--- assignment-6/gen_tests.py
+++ assignment-6/gen_tests.py
@@ -1,7 +1,8 @@
-#! /usr/bin/python3
+#! /usr/bin/env python3
 from pathlib import Path
 from glob import glob
 import argparse
+import sys
 
 """
 Looks at all `.diamond` files in the directory.
@@ -23,31 +24,33 @@ test1.out:
     6
 """
 
-def walk_dir(delimiter=r';',dirname:str='.', filetype='.diamond', verbose=False) -> None :
-    delimiter = f'\n{delimiter}\n'
-    for filepath in glob(f'**/*{filetype}', root_dir=dirname):
+
+def walk_dir(
+    delimiter=r";", dirname: str = ".", filetype=".diamond", verbose=False
+) -> None:
+    delimiter = f"\n{delimiter}\n"
+    for filepath in glob(f"**/*{filetype}", root_dir=dirname):
         testfile = dirname / Path(filepath)
-        suffix = '.err' if 'do_err' in str(testfile) else '.out'
+        suffix = ".err" if "do_err" in str(testfile) else ".out"
 
         resultfile = testfile.with_suffix(suffix)
         if resultfile.exists():
             if verbose:
-                print(f'{str(testfile):.<50}{str(resultfile)} already exists.')
+                print(f"{str(testfile):.<50}{str(resultfile)} already exists.")
         else:
             if verbose:
-                print(f'{str(testfile):.<50}', end='')
-            with open(testfile, 'r+') as tf:
+                print(f"{str(testfile):.<50}", end="")
+            with open(testfile, "r+") as tf:
                 contents = tf.read()
                 test, result = contents.split(delimiter, maxsplit=1)
                 tf.seek(0)
                 tf.write(test)
                 tf.truncate()
 
-            with open(resultfile, 'w') as rf:
+            with open(resultfile, "w") as rf:
                 rf.write(result)
             if verbose:
-                print(f'{str(resultfile)} generated!')
-
+                print(f"{str(resultfile)} generated!")
 
 
 def main(args):
@@ -55,15 +58,17 @@ def main(args):
         delimiter=args.splitter,
         dirname=args.dir,
         verbose=args.verbose,
-        filetype=args.filetype)
+        filetype=args.filetype,
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-d', '--dir', type=str, default='input/')
-    parser.add_argument('-s', '--splitter', type=str, default=';')
-    parser.add_argument('-v', '--verbose', action="store_true", default=False)
-    parser.add_argument('-f', '--filetype', type=str, default='.diamond')
+    parser.add_argument("-d", "--dir", type=str, default="input/")
+    parser.add_argument("-s", "--splitter", type=str, default=";")
+    parser.add_argument("-v", "--verbose", action="store_true", default=False)
+    parser.add_argument("-f", "--filetype", type=str, default=".diamond")
 
     args = parser.parse_args()
     main(args)
